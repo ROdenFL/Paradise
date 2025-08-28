6029477302c4d4df740eed91eab34d6ff49b52a1
ROdenFL <144662735+ROdenFL@users.noreply.github.com>
2025-08-28 20:48:52 +0300

    check8

--- tools/ci/check_grep.sh
+++ tools/ci/check_grep.sh
@@ -224,7 +224,7 @@ if grep -P '^/*var/' code/**/*.dm; then
 	echo
     st=1
 fi;
-if grep -n '#define FAST_LOAD' _maps/common.dm | grep -v '//#define FAST_LOAD'; then
+if grep '#define FAST_LOAD' _maps/common.dm | grep -v '\/\/#define FAST_LOAD'; then
     echo -e "${RED}ERROR: commiting uncommented FAST_LOAD define.${NC}"
 	echo
     st=1
