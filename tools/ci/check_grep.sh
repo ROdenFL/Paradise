ab198680893bb6a28222be0ef63a9369ca121bfc
ROdenFL <144662735+ROdenFL@users.noreply.github.com>
2025-08-28 20:21:39 +0300

    check2

--- tools/ci/check_grep.sh
+++ tools/ci/check_grep.sh
@@ -224,7 +224,7 @@ if grep -P '^/*var/' code/**/*.dm; then
 	echo
     st=1
 fi;
-if grep -P '(?<!//)#define FAST_LOAD' code/**/*.dm; then
+if grep -rP '(?<!//)#define FAST_LOAD' *.dm; then
     echo -e "${RED}ERROR: commiting uncommented FAST_LOAD define.${NC}"
 	echo
     st=1
